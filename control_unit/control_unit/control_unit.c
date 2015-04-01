8c32b5713d6b51e560a8d568df53ccc9679e166a
Lage Ragnarsson <lage.ragnarsson@gmail.com>
2015-04-01 19:36:43 +0200

    Added some basic state transitions, feedback control methods, include file and comments

--- control_unit/control_unit/control_unit.c
+++ control_unit/control_unit/control_unit.c
@@ -1,83 +1,108 @@
 /*
  * control_unit.c
- *
- * Created: 3/24/2015 9:06:14 AM
- *  Author: lagra033
  */
- 
-#ifndef F_CPU
-#define F_CPU 20000000UL
-#endif
-#define ENGINE_LEFT_SPEED OCR0A
-#define ENGINE_RIGHT_SPEED OCR0B
-#define ENGINE_LEFT_DIRECTION PB0
-#define ENGINE_RIGHT_DIRECTION PB1
-#define LEFT 1
-#define BACKWARD 2
-#define RIGHT 3
+#include "control_unit.h"
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-
-const uint8_t MAPPING_SPEED = 200;
-const uint8_t SUPER_SPEED = 150;
-const uint8_t TURN_SPEED = 150;
-
-typedef enum { NORTH, EAST, SOUTH, WEST } direction;
-static const uint8_t ROUTE_END = 10;
-
-direction current_direction;
-direction current_route[64]  = { NORTH, NORTH, NORTH, NORTH, EAST, EAST, SOUTH, WEST, WEST, WEST, EAST, 10 };
-
-uint8_t route_index = 0;
-uint16_t distance_remaining = 0;
-uint8_t angle_remaining  = 0;
-
-uint8_t current_speed;
-
+/* Initialize all hardware ports */
 void init_ports() {
-	TCCR0A = (0<<WGM01)|(1<<WGM00)|(1<<COM0A1)|(0<<COM0A0)|(1<<COM0B1)|(0<<COM0B0);
-	TCCR0B = (0<<WGM02)|(1<<CS02)|(0<<CS01)|(0<<CS00);
-	DDRB = (1<<DDB0)|(1<<DDB1)|(1<<DDB3)|(1<<DDB4);	
-	DDRD = 0<<DDD1;
+	TCCR0A = (0<<WGM01)|(1<<WGM00)|(1<<COM0A1)|(0<<COM0A0)|(1<<COM0B1)|(0<<COM0B0); // PWM for left and right engine
+	TCCR0B = (0<<WGM02)|(1<<CS02)|(0<<CS01)|(0<<CS00); // PWM for left and right engine
+	DDRB = (1<<DDB0)|(1<<DDB1)|(1<<DDB3)|(1<<DDB4);	// Left and right engine speed and direction as output
+	DDRD = 0<<DDD1; // Input, reset button
 }
 
+/* Set current speed to predefined value and update control_speed_max */
+void set_current_speed(uint8_t speed) {
+	current_speed = speed;
+	control_speed_max = 0.49 * speed;
+}
+
+/* Initialize starting conditions for the robot */
 void init_state() {
+	current_mode = AUTONOMOUS;
+	current_task = SEARCH;
 	current_direction = NORTH;
-	current_speed = MAPPING_SPEED;
+	set_current_speed(MAPPING_SPEED);
 }
 
+/* Stop engines by settings PWM duty cycles to 0. Direction unaffected */
 void stop_engines() {
 	ENGINE_LEFT_SPEED =	ENGINE_RIGHT_SPEED = 0;
 }
 
+/* Absolute value of linear approximation of left "angle" derivative */
+uint8_t get_angle_change_left() {
+	if (current_angle_left > last_tick_angle_left)
+		return inverse_sampling_speed * (current_angle_left - last_tick_angle_left);
+	return inverse_sampling_speed * (last_tick_angle_left - current_angle_left);
+}
+
+/* Absolute value of linear approximation of right "angle" derivative */
+uint8_t get_angle_change_right() {
+	if (current_angle_right > last_tick_angle_right)
+		return inverse_sampling_speed * (current_angle_right - last_tick_angle_right);
+	return inverse_sampling_speed * (last_tick_angle_right - current_angle_right);
+}
+
+/* Calculates the control_speed based on current angle and error in distance to the middle of the corridor */
+void calculate_control_speed() {
+	uint16_t control_speed_16 = P_COEFFICIENT * current_distance_error + D_COEFFICIENT * current_angle;
+	if (control_speed_16 > control_speed_max)
+		control_speed = control_speed_max;
+	else
+		control_speed = (uint8_t) control_speed_16;
+}
+
+/* Drive forward until distance_remaining is 0 or until entering a corridor from a crossroad-section. */
 void step_forward() {
 	PORTB = (1<<ENGINE_LEFT_DIRECTION)|(1<<ENGINE_RIGHT_DIRECTION);
 	ENGINE_LEFT_SPEED = ENGINE_RIGHT_SPEED = current_speed;
-	while(distance_remaining != 0) {
-		_delay_ms(5);
+	while (distance_remaining != 0) {
 		--distance_remaining;
+		if (get_angle_change_left() > ANGLE_CHANGE_THRESHOLD || get_angle_change_right() > ANGLE_CHANGE_THRESHOLD)
+			return; // Reentered the corridor
+		_delay_ms(2);
 	}
-	stop_engines();	
+	stop_engines();
 }
 
+/* Drive forward until distance_remaining is 0. Will temporarily switch to step_forward to turn off sensor feedback if passing a crossroad. */
 void drive_forward() {
 	PORTB = (1<<ENGINE_LEFT_DIRECTION)|(1<<ENGINE_RIGHT_DIRECTION);
-	ENGINE_LEFT_SPEED = ENGINE_RIGHT_SPEED = current_speed;
-	while(distance_remaining != 0) {
-		_delay_ms(5);
-		--distance_remaining;
+	while (distance_remaining != 0) {
+		calculate_control_speed();
+		ENGINE_LEFT_SPEED = current_speed - control_speed;
+		ENGINE_RIGHT_SPEED = current_speed + control_speed;
+		--distance_remaining; // Should be controlled by wheel encoders
+		if (get_angle_change_left() > ANGLE_CHANGE_THRESHOLD || get_angle_change_right() > ANGLE_CHANGE_THRESHOLD)
+			step_forward(); // Entered a crossroad section (turn off sensor feedback temporarily)
+		_delay_ms(2);
 	}
-	stop_engines();	
+	stop_engines();
 }
 
+/* Drive forward and map surroundings until reaching a wall. Will temporarily switch to step_forward to turn off sensor feedback if passing a crossroad. */
+void drive_and_map() {
+	PORTB = (1<<ENGINE_LEFT_DIRECTION)|(1<<ENGINE_RIGHT_DIRECTION);
+	while (distance_remaining != 0) {
+		calculate_control_speed();
+		ENGINE_LEFT_SPEED = current_speed - control_speed;
+		ENGINE_RIGHT_SPEED = current_speed + control_speed;
+		--distance_remaining; // Should be controlled by wheel encoders
+		if (get_angle_change_left() > ANGLE_CHANGE_THRESHOLD || get_angle_change_right() > ANGLE_CHANGE_THRESHOLD)
+		step_forward(); // Entered a crossroad section (turn off sensor feedback temporarily)
+		_delay_ms(2);
+	}
+	stop_engines();
+}
+
+/* Rotate 90 degrees to the left based on wheel encoder feedback */
 void rotate_left_90() {
 	angle_remaining = 90;
 	PORTB = (0<<ENGINE_LEFT_DIRECTION)|(1<<ENGINE_RIGHT_DIRECTION);
 	ENGINE_LEFT_SPEED = ENGINE_RIGHT_SPEED = TURN_SPEED;
 	while(angle_remaining != 0) {
-		_delay_ms(5);
+		_delay_ms(3);
 		-- angle_remaining; // simulering
 	}
 	--current_direction;
@@ -85,12 +110,13 @@ void rotate_left_90() {
 	stop_engines();	
 }
 
+/* Rotate 90 degrees to the right based on wheel encoder feedback */
 void rotate_right_90() {
 	angle_remaining = 90;
 	PORTB = (1<<ENGINE_LEFT_DIRECTION)|(0<<ENGINE_RIGHT_DIRECTION);
 	ENGINE_LEFT_SPEED = ENGINE_RIGHT_SPEED = TURN_SPEED;
 	while(angle_remaining != 0) {
-		_delay_ms(5);
+		_delay_ms(3);
 		-- angle_remaining; // simulering
 	}
 	++current_direction;
@@ -98,12 +124,13 @@ void rotate_right_90() {
 	stop_engines();	
 }
 
+/* Rotate 180 degrees based on wheel encoder feedback */
 void rotate_180() {
 	angle_remaining = 180;
 	PORTB = (1<<ENGINE_LEFT_DIRECTION)|(0<<ENGINE_RIGHT_DIRECTION);
 	ENGINE_LEFT_SPEED = ENGINE_RIGHT_SPEED = TURN_SPEED;
 	while(angle_remaining != 0) {
-		_delay_ms(5);
+		_delay_ms(3);
 		-- angle_remaining; // simulering
 	}
 	current_direction += 2;
@@ -111,49 +138,108 @@ void rotate_180() {
 	stop_engines();
 }
 
+/* Creates and sets current_route based on map data */
 void create_route() {
-	
 	
 }
 
-uint8_t navigate() {
-	direction next_direction = current_route[route_index];
-	if (next_direction == ROUTE_END)
-		return 0;
-	distance_remaining = 0;
-	do {
-		distance_remaining += 100;
-		++route_index;
-	} while (next_direction == current_route[route_index]);
+/* Contains all logic for mapping and searching through the map */
+void search_state() {
+	set_current_speed(MAPPING_SPEED);
+	route_index = 0;
+	current_task = RETRIEVE;
+}
 
-	uint8_t next_turn = (current_direction - next_direction) & 3;
-	switch (next_turn) {
-		case LEFT:
-			rotate_left_90();
-			step_forward();
+/* Ronny is going to retrieve the package but wants to make sure he knows the shortest path first */
+void retrieve_state() {
+	set_current_speed(SUPER_SPEED);
+	know_shortest_path = 1;
+}
+
+/* Ronny is back at the start to pick up the package (in a shady way) */
+void grab_package_state() {
+	route_index = 0;
+	current_task = DELIVER;	
+}
+
+/* Drops the package and positions Ronny to avoid running it over later */
+void drop_package_state() {
+	route_index = 0;
+	current_task = RETURN;	
+}
+
+/* Ronny has reached the goal and does a victory dance */
+void end_state() {
+	
+}
+
+/* Follows the current path until the end and then selects state transition */
+state_function navigate() {
+	direction next_direction = current_route[route_index];
+	/* Follow current path */
+	while(next_direction != ROUTE_END) {
+		distance_remaining = 0;
+		/* Calculate straight line distance remaining in current direction */
+		do {
+			distance_remaining += 100;
+			++route_index;
+		} while (next_direction == current_route[route_index]);
+		/* Decide what turn to make based on current direction and next direction in route */
+		uint8_t next_turn = (current_direction - next_direction) & 3;
+		switch (next_turn) {
+			case LEFT:
+				rotate_left_90();
+				step_forward();
+				break;
+			case RIGHT:
+				rotate_right_90();
+				step_forward();
+				break;
+			case BACKWARD:
+				rotate_180();
+				break;
+			default:
+				break;
+		}
+		drive_forward();
+		next_direction = current_route[route_index];
+	}
+
+	/* Figure out the correct state transition*/
+	state_function state_transition;
+	switch (current_task) {
+		case SEARCH:
+			state_transition = &search_state; // Navigated to a un-mapped area, keep searching.
 			break;
-		case RIGHT:
-			rotate_right_90();
-			step_forward();
+		case RETRIEVE:
+			if (know_shortest_path)
+				state_transition = &grab_package_state;  // The last route was the shortest path to the start - grab package.
+			else
+				state_transition = &retrieve_state;  // The shortest path between the goal and start is not yet known, the last route was to an unmapped area - keep looking for shortest path.
 			break;
-		case BACKWARD:
-			rotate_180();
+		case DELIVER:
+			state_transition = &drop_package_state; // The last route was from start to the goal - drop the package.
+			break;
+		case RETURN:
+			state_transition = &end_state; // The last route was from goal to start - do a victory dance!
 			break;
 		default:
+			state_transition = &end_state; // Should never happen.
 			break;
 	}
-	
-	drive_forward();
-	return 1;
+	return state_transition;
 }
 
 int main(void) {
 	init_ports();
 	init_state();
 
-	uint8_t status = 0;
+	state_function state_transition;
+	// ## TEMP_COMMENT ## Will currently follow the predefined path 4 times in a row since no new path is ever calculated for next task.
 	do {
-		status = navigate();
-	} while (status == 1);
+		state_transition = navigate();
+		(*state_transition)();
+	} while (current_task != IDLE);
+	
 	stop_engines();
 }
