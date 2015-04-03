ecad3bfa7158bf07a7edcd33f78c8b7f1111ad7a
Lage Ragnarsson <lage.ragnarsson@gmail.com>
2015-04-04 00:26:58 +0200

    Added personality to Ronny

--- communication_unit/communication_unit/communication_unit.c
+++ communication_unit/communication_unit/communication_unit.c
@@ -6,27 +6,48 @@
  */ 
 #include <avr/io.h>
 #define F_CPU 18432000UL
-#include <avr/interrupt.h>
+//#include <avr/interrupt.h>
 #include <util/delay.h>
+#include "personality.h"
 
-#include "UART.h"
-#include "I2C.h"
+//#include "UART.h"
+//#include "I2C.h"
+//#include "sound.c"
 
+void init_personality() {
+	TCCR1A = (0<<WGM11)|(0<<WGM10)|(1<<COM1A1)|(0<<COM1A0);
+	TCCR1B = (1<<WGM13)|(0<<WGM12)|(0<<CS12)|(1<<CS11)|(0<<CS10);
+	DDRD = (1<<DDD5);
+	OCR1AH = (100 >> 8);
+	OCR1AL = 100;
+
+	for (int i=0; i < 250; i++) {
+		ICR1H = (imperial_march[i] >> 8);
+		ICR1L = imperial_march[i];
+		++i;
+		delay_ms(imperial_march[i]);
+		ICR1H = (0 >> 8);
+		ICR1L = 0;
+		++i;
+		delay_ms(imperial_march[i]);		
+	}
+}
 
 int main(void) {
-	Init_UART(9); //Set baudrate to 115.2kbps and initiate UART
-	i2c_init(atmega18br, atmega18pc, communication_unit);
+	//Init_UART(9); //Set baudrate to 115.2kbps and initiate UART
+	//i2c_init(atmega18br, atmega18pc, communication_unit);
+	//init_personality();
 	while(1) {
-		/*if(UART_not_empty()) {
+		//if(UART_not_empty()) {
 			//uint8_t data = UART_get_buffer();
 			//UART_putstring(data);
 			//Send_to_PC('\n');
-			i2c_send(sensor_unit, 0x52);
-			UART_empty();
-		}*/
-		_delay_ms(1000);
-		i2c_send(sensor_unit, 0x41);
-		_delay_ms(5000);
+			//i2c_send(sensor_unit, 0x52);
+			//UART_empty();
+		//}
+		//_delay_ms(1000);
+		//i2c_send(sensor_unit, 0x41);
+		//_delay_ms(5000);
 	}
 	return 0;
 }
---
+++ communication_unit/communication_unit/personality.h
@@ -0,0 +1,92 @@
+﻿/*
+ * personality.h
+ *
+ * Created: 4/4/2015 12:21:55 AM
+ *  Author: lagra033
+ */ 
+
+
+#ifndef PERSONALITY_H_
+#define PERSONALITY_H_
+
+#define F_CPU 18432000UL
+#include <util/delay.h>
+
+#define C4 8806
+#define Db4 8312
+#define D4 7846
+#define Eb4 7405
+#define E4 6990
+#define F4 6597
+#define Gb4 6227
+#define G4 5878
+#define Ab4 5548
+#define A4 5236
+#define Bb4 4943
+#define B4 4665
+#define C5 4403
+#define Db5 4156
+#define D5 3923
+#define Eb5 3703
+#define E5 3495
+#define F5 3299
+#define Gb5 3114
+#define G5 2939
+#define Ab5 2774
+#define A5 2618
+#define Bb5 2471
+#define B5 2332
+#define C6 2202
+#define Db6 2078
+#define D6 1961
+#define Eb6 1851
+#define E6 1747
+#define F6 1649
+#define Gb6 1557
+#define G6 1496
+#define Ab6 1387
+#define A6 1309
+#define Bb6 1236
+#define B6 1166
+#define C7 1101
+#define Db7 1039
+#define D7 981
+#define Eb7 926
+#define E7 874
+#define F7 825
+#define Gb7 778
+#define G7 735
+#define Ab7 693
+#define A7 655
+#define Bb7 618
+#define B7 583
+#define C8 550
+
+#define BPM 200
+#define H 2*Q
+#define Q 60000/BPM
+#define E Q/2
+#define S Q/4
+#define W 4*Q
+
+void delay_ms(uint16_t count) {
+	while(count--) {
+		_delay_ms(1);
+
+	}
+}
+
+uint16_t imperial_march[250] = {A4, Q, Q, A4, Q, Q, A4, Q, Q, F4, E+S, E+S, C5, S, S, A4, Q, Q,
+								F4, E+S, E+S, C5, S, S, A4, H, H, E5, Q, Q, E5, Q, Q, E5, Q, Q,
+								F5, E+S, E+S, C5, S, S, Ab4, Q, Q, F4, E+S, E+S, C5, S, S,
+								A4, H, H, A5, Q, Q, A4, E+S, E+S, A4, S, S, A5, Q, Q, Ab5, E+S, E+S,
+								G5, S, S, Gb5, S, S, E5, S, S, F5, E, E+Q, Bb4, E, E, Eb5, Q, Q,
+								D5, E+S, E+S, Db5, S, S, C5, S, S, B4, S, S, C5, E, E+Q, F4, E, E,
+								Ab4, Q, Q, F4, E+S, E+S, A4, S, S, C5, Q, Q, A4, E+S, E+S, C5, S, S,
+								E5, H, H, A5, Q, Q, A4, E+S, E+S, A4, S, S, A5, Q, Q, Ab5, E+S, E+S,
+								G5, S, S, Gb5, S, S, E5, S, S, F5, E, E+Q, Bb4, E, E, Eb5, Q, Q,
+								D5, E+S, E+S, Db5, S, S, C5, S, S, B4, S, S, C5, E, E+Q, F4, E, E,
+								Ab4, Q, Q, F4, E+S, E+S, C5, S, S, A4, Q, Q, F4, E+S, E+S, C5, S, S,
+								A4, H, H};
+
+#endif /* PERSONALITY_H_ */
