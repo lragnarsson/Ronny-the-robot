27089955ab7fbc15b89b3792389d12cb340884b8
Jesper Otterholm <jesot351@student.liu.se>
2015-05-22 11:17:27 +0200

    improved uart with multiple bytes from pc

--- communication_unit/communication_unit/UART.h
+++ communication_unit/communication_unit/UART.h
@@ -7,16 +7,16 @@
 
 #ifndef UART_H_
 #define UART_H_
-/*volatile int i=0;
-volatile uint8_t buffer[20];
-volatile uint8_t StrRxFlag=0;*/
-volatile uint8_t buffer[20];
-uint8_t UART_not_empty(void);
+
+volatile uint8_t buffer[8];
+
 void UART_get_buffer(uint8_t* data);
 void UART_empty(void);
+uint8_t UART_not_empty();
 void Init_UART(uint16_t baudrate);
 void UART_putstring(unsigned char* StringPtr);
 void Send_to_PC(unsigned char data);
-unsigned char Receive_from_PC(void);
+uint8_t Receive_from_PC(void);
 void UART_Flush(void);
+
 #endif /* UART_H_ */
